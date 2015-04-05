dbca9dff214c0fa08ec910d40794fd0031f6294a
Matteo Calosci <matteo1789@live.it>
2015-04-05 17:49:43 +0200

    First

---
+++ Insertion-Sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5
+
+int main(){
+int v[N];
+int n,x,i = 0;
+
+printf("Inserisci l'elemento %d: \n", i);
+scanf("%d", &v[0]);
+
+      
+for(x = 1; x < N; x++) {
+      printf("Inserisci il valore da mettere nell'array: \n");
+      scanf("%d", &n);
+      for(i = x; n < v[i - 1] && i > 0; i--) {
+           v[i] = v[i - 1];
+      }
+      v[i] = n;
+}
+
+printf("\n\n");
+
+for(i = 0; i < N; i++) {
+      printf("Elemento %d dell'array ordinato\n",i,v[i]);
+      }      
+
+system("pause");
+return (EXIT_SUCCESS);  
+}
